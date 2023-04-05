ab151826cda8d4d24aa74c8920b5e7214a9c7475
Etienne Studer <etienne@gradle.com>
2023-04-05 07:35:05 +0200

    Remove fallback for github.com

--- src/main/java/com/gradle/CustomBuildScanEnhancements.java
+++ src/main/java/com/gradle/CustomBuildScanEnhancements.java
@@ -270,10 +270,10 @@ final class CustomBuildScanEnhancements {
             }
 
             if (isGitHubActions(providers)) {
-                String gitHubUrl = envVariable("GITHUB_SERVER_URL", providers).orElse("https://github.com");
+                Optional<String> gitHubUrl = envVariable("GITHUB_SERVER_URL", providers);
                 Optional<String> gitHubRepository = envVariable("GITHUB_REPOSITORY", providers);
                 Optional<String> gitHubRunId = envVariable("GITHUB_RUN_ID", providers);
-                if (gitHubRepository.isPresent() && gitHubRunId.isPresent()) {
+                if (gitHubUrl.isPresent() && gitHubRepository.isPresent() && gitHubRunId.isPresent()) {
                     buildScan.link("GitHub Actions build", gitHubUrl + "/" + gitHubRepository.get() + "/actions/runs/" + gitHubRunId.get());
                 }
                 envVariable("GITHUB_WORKFLOW", providers).ifPresent(value ->
