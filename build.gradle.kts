e6cc33b8f492f8e32a860a3606b1e0f8251293e7
Jerome Prinet <jprinet@gradle.com>
2025-02-24 11:22:56 +0100

    Add required dependency on junit-platform-launcher
    See https://github.com/junit-team/junit5/issues/4335

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,8 @@ dependencies {
 
     testImplementation(platform("org.junit:junit-bom:5.12.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 wrapperUpgrade {
