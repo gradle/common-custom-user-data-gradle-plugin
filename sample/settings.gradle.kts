8e0c9f9da96b37962cb7595e4089c68a50f8c414
Gasper Kojek <gkojek@gradle.com>
2025-07-09 10:24:57 +0200

    Added a sample project

---
+++ sample/settings.gradle.kts
@@ -0,0 +1,42 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    // Apply the foojay-resolver plugin to allow automatic download of JDKs
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("com.gradle.develocity") version "4.0.2"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2+" // Use the latest version of the plugin
+}
+
+val isCI = System.getenv("CI") != null
+
+develocity {
+    server = "https://ge.solutions-team.gradle.com"
+    buildScan {
+        uploadInBackground = !isCI
+        publishing.onlyIf { it.isAuthenticated }
+        obfuscation {
+            ipAddresses { addresses -> addresses.map { _ -> "0.0.0.0" } }
+        }
+    }
+}
+
+buildCache {
+    local {
+        isEnabled = true
+    }
+
+    remote(develocity.buildCache) {
+        isEnabled = true
+        // Check access key presence to avoid build cache errors on PR builds when access key is not present
+        val accessKey = System.getenv("DEVELOCITY_ACCESS_KEY")
+        isPush = isCI && accessKey != null
+    }
+}
+
+rootProject.name = "ccud-gradle-sample"
+include("app")
