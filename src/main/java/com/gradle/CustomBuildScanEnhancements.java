b6f2d520d4c34b78af023c4b7ffbf9eb59d3eaf0
Eric Haag <ehaag@gradle.com>
2025-08-29 14:51:30 -0500

    Add better support for GitHub Actions reruns

--- src/main/java/com/gradle/CustomBuildScanEnhancements.java
+++ src/main/java/com/gradle/CustomBuildScanEnhancements.java
@@ -294,26 +294,47 @@ final class CustomBuildScanEnhancements {
 
             if (isGitHubActions(providers)) {
                 buildScan.value("CI provider", "GitHub Actions");
-                Optional<String> gitHubUrl = envVariable("GITHUB_SERVER_URL", providers);
+
+                Optional<String> workflow = envVariable("GITHUB_WORKFLOW", providers);
+                Optional<String> jobId = envVariable("GITHUB_JOB", providers);
+                Optional<String> actionNameOrStepId = envVariable("GITHUB_ACTION", providers);
+                Optional<String> runId = envVariable("GITHUB_RUN_ID", providers);
+                Optional<String> runAttempt = envVariable("GITHUB_RUN_ATTEMPT", providers);
+                Optional<String> runNumber = envVariable("GITHUB_RUN_NUMBER", providers);
+                Optional<String> headRef = envVariable("GITHUB_HEAD_REF", providers).filter(value -> !value.isEmpty());
+                Optional<String> serverUrl = envVariable("GITHUB_SERVER_URL", providers);
                 Optional<String> gitRepository = envVariable("GITHUB_REPOSITORY", providers);
-                Optional<String> gitHubRunId = envVariable("GITHUB_RUN_ID", providers);
-                if (gitHubUrl.isPresent() && gitRepository.isPresent() && gitHubRunId.isPresent()) {
-                    buildScan.link("GitHub Actions build", gitHubUrl.get() + "/" + gitRepository.get() + "/actions/runs/" + gitHubRunId.get());
-                }
-                envVariable("GITHUB_WORKFLOW", providers).ifPresent(value ->
-                    addCustomValueAndSearchLink(develocity, "CI workflow", value));
-                envVariable("GITHUB_RUN_ID", providers).ifPresent(value ->
-                    addCustomValueAndSearchLink(develocity, "CI run", value));
-                envVariable("GITHUB_RUN_ATTEMPT", providers).ifPresent(value ->
-                        buildScan.value("CI run attempt", value));
-                envVariable("GITHUB_RUN_NUMBER", providers).ifPresent(value ->
-                        buildScan.value("CI run number", value));
-                envVariable("GITHUB_ACTION", providers).ifPresent(value ->
-                        addCustomValueAndSearchLink(develocity, "CI step", value));
-                envVariable("GITHUB_JOB", providers).ifPresent(value ->
+
+                workflow.ifPresent(value ->
+                        addCustomValueAndSearchLink(develocity, "CI workflow", value));
+                jobId.ifPresent(value ->
                         addCustomValueAndSearchLink(develocity, "CI job", value));
-                envVariable("GITHUB_HEAD_REF", providers).filter(value -> !value.isEmpty()).ifPresent(value ->
+                actionNameOrStepId.ifPresent(value ->
+                        addCustomValueAndSearchLink(develocity, "CI step", value));
+
+                runId.ifPresent(value ->
+                        buildScan.value("CI run", value));
+                runAttempt.ifPresent(value ->
+                        buildScan.value("CI run attempt", value));
+                runNumber.ifPresent(value ->
+                        buildScan.value("CI run number", value));
+                headRef.ifPresent(value ->
                         buildScan.value("PR branch", value));
+
+                if (serverUrl.isPresent() && gitRepository.isPresent() && runId.isPresent()) {
+                    StringBuilder githubActionsBuild = new StringBuilder(serverUrl.get())
+                            .append("/").append(gitRepository.get())
+                            .append("/actions/runs/").append(runId.get());
+                    runAttempt.ifPresent(value -> githubActionsBuild.append("/attempts/").append(value));
+                    buildScan.link("GitHub Actions build", githubActionsBuild.toString());
+                }
+
+                if (runId.isPresent()) {
+                    Map<String, String> params = new HashMap<>();
+                    params.put("CI run", runId.get());
+                    runAttempt.ifPresent(value -> params.put("CI run attempt", value));
+                    addSearchLink(develocity, "CI run", params);
+                }
             }
 
             if (isGitLab(providers)) {
